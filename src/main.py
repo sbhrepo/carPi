59e74d1d88857def71bc77bf9d8fb4279deca8a7
S BH <s_b_h@hotmail.com>
2022-12-05 13:45:18 -0500

    undo the inherit change

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import drive
 import navigate
 from flask import Flask
 from flask import request
@@ -7,81 +8,82 @@ app = Flask(__name__)
 
 @app.route('/status', methods=['GET'])
 def status():
-    return nav.status()
+    return drive.status()
 
 @app.route('/control/shutdown', methods=['GET'])
 def shutdown():
-    nav.stop()
-    nav.powerOff()
+    drive.stop()
+    drive.powerOff()
     call("sudo shutdown --n", shell=True)
     return f"Raspberry Pi Shutdown"
 
 @app.route('/control/poweron', methods=['GET'])
 def poweron():
-    return nav.powerOn()
+    return drive.powerOn()
 
 @app.route('/control/poweroff', methods=['GET'])
 def poweroff():
-    return nav.powerOff()
+    return drive.powerOff()
 
 @app.route('/drive/stop', methods=['GET'])
 def stop():
-    return nav.stop()
+    return drive.stop()
 
 @app.route('/drive/front/<speed>', methods=['GET'])
 def front(speed):
-    return nav.front(speed, 0)
+    return drive.front(speed, 0)
 
 @app.route('/drive/back/<speed>', methods=['GET'])
 def back(speed):
-    return nav.back(speed, 0)
+    return drive.back(speed, 0)
 
 @app.route('/drive/right/<speed>', methods=['GET'])
 def right(speed):
-    return nav.right(speed, 0)
+    return drive.right(speed, 0)
 
 @app.route('/drive/left/<speed>', methods=['GET'])
 def left(speed):
-    return nav.left(speed, 0)
+    return drive.left(speed, 0)
 
 @app.route('/drive/frontRight/<speed>', methods=['GET'])
 def frontRight(speed):
-    return nav.frontRight(speed, 0)
+    return drive.frontRight(speed, 0)
 
 @app.route('/drive/frontLeft/<speed>', methods=['GET'])
 def frontLeft(speed):
-    return nav.frontLeft(speed, 0)
+    return drive.frontLeft(speed, 0)
 
 @app.route('/drive/backRight/<speed>', methods=['GET'])
 def backRight(speed):
-    return nav.backRight(speed, 0)
+    return drive.backRight(speed, 0)
 
 @app.route('/drive/backLeft/<speed>', methods=['GET'])
 def backLeft(speed):
-    return nav.backLeft(speed, 0)
+    return drive.backLeft(speed, 0)
 
 @app.route('/drive/turnRight/<speed>', methods=['GET'])
 def turnRight(speed):
-    return nav.turnRight(speed, 0)
+    return drive.turnRight(speed, 0)
 
 @app.route('/drive/turnLeft/<speed>', methods=['GET'])
 def turnLeft(speed):
-    return nav.turnLeft(speed, 0)
+    return drive.turnLeft(speed, 0)
 
 @app.route('/navigate/stop', methods=['GET'])
 def NavigateStop():    
-    return nav.stoptRoute()
+    return drive.stoptRoute()
 
 @app.route('/navigate/start/<name>', methods=['GET'])
 def navigateTo(name):
     speed = int(request.args.get("speed"))
     timeToRun = int(request.args.get("time"))
     print("name=",name," speed=",speed," time=",timeToRun)
-    return nav.startRoute(name, speed, timeToRun)
+    return drive.startRoute(name, speed, timeToRun)
 
 
 if __name__ == '__main__':
-    nav = navigate.Navigate()
+    drive = drive.Drive()
+    nav = navigate.Navigate(drive)
     app.run(host='0.0.0.0', port=8090)
-    nav.stop()
-    nav.powerOff()
+    drive.stop()
+    drive.powerOff()
--- src/navigate.py
+++ src/navigate.py
@@ -1,34 +1,32 @@
-import drive
-import control
-import motor
 import time
 
-class Navigate(drive.Drive):
-    def __init__(self):
+class Navigate():
+    def __init__(self, drive):
         super().__init__()
-        self.isStop = True  
+        self.drive = drive
+        self.isStop = True
 
     def startRoute(self, routeName, speed, timeToRun):
         self.isStop = False        
         if routeName == "BackAndForth":
             print("B&F ", routeName, " speed=", speed, " time=", timeToRun)
             while(self.isStop == False):
-                self.front(speed, 0)
+                self.drive.front(speed, 0)
                 counter = 0
                 while(counter < timeToRun): # sleep loop to stop in middle if needed
                     counter += 1
                     time.sleep(1)
                     if self.isStop:
-                        self.stop()
+                        self.drive.stop()
                         return f"Route Stopped in middle"
                 print("in backand forth back")
-                self.back(speed, 0)
+                self.drive.back(speed, 0)
                 counter = 0
                 while(counter < timeToRun): # sleep loop to stop in middle if needed
                     counter += 1
                     time.sleep(1)
                     if self.isStop:
-                        self.stop()
+                        self.drive.stop()
                         return f"Route Stopped in middle"
         return f"Route {routeName} did not start"
 
